7d136705fe0ebcc5aba58eda72f8d1071b93ac68
Eric Drechsel <eric@pdxhub.org>
2014-01-06 00:14:36 -0800

    update test stack

--- test/karma-unit.conf.js
+++ test/karma-unit.conf.js
@@ -1,10 +1,13 @@
 module.exports = function(config) {
   config.set({
     files : [
+      'bower_components/lodash/dist/lodash.js',
+      'bower_components/pouchdb-nightly.min.js/index.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-pouchdb/angular-pouchdb.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'app/scripts/homePages.js',
+      'app/scripts/pages.js',
       'app/scripts/app.js',
       'test/unit/**/*.js'
     ],
