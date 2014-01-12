1c9226105f9de1f403504b591938444d3849bcb5
Eric Drechsel <eric@pdxhub.org>
2014-01-12 01:51:52 -0800

    rename some css ids+classes

--- test/e2e/draftsSpec.js
+++ test/e2e/draftsSpec.js
@@ -1,23 +1,28 @@
 describe('Drafts', function() {
   it('should allow editing title and body', function() {
-    browser.get('/');
+    browser.get('');
 
     expect($('#new-page').isDisplayed()).toBe(true);
     expect($('#draft-page').isDisplayed()).toBe(false);
 
     $('#new-page').click();
-    $('#draftTitle').click();
-    $('#draftTitle').sendKeys("Foo");
-    $('#draftBody').click();
-    $('#draftBody').sendKeys("Bar");
-    expect($('#draftTitle').getText()).toBe('Foo');
-    expect($('#draftBody').getText()).toBe('Bar');
+    $('#title').click();
+    $('#title').sendKeys("Foo");
+    $('#body').click();
+    $('#body').sendKeys("Bar");
+    expect($('#title').getText()).toBe('Foo');
+    expect($('#body').getText()).toBe('Bar');
 
     // Check that saving locally works
     // Back
     browser.navigate().back();
 
-    // check new page is hidden and draft is shown
-    // check draft title
+    // expect($('#new-page').isDisplayed()).toBe(false);
+    // expect($('#draft-page').isDisplayed()).toBe(true);
+
+    $('#new-page').click();
+    
+    expect($('#title').getText()).toBe('Foo');
+    expect($('#body').getText()).toBe('Bar');
   });
 });
